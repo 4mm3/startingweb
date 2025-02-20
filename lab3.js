6cc9f29464b1cba879374aa80b0629456892e719
4mm3 <emmaferreira@ocadu.ca>
2025-02-20 15:07:12 -0500

    Lab 3

---
+++ lab3.js
@@ -0,0 +1,56 @@
+var colourPicker; // function scope
+let strokeWeightSlider;
+var bgColourPicker;
+var drawRadio;
+
+function setup(){
+    createCanvas(720,300);
+    colourPicker = createColorPicker('green');
+    
+    strokeWeightSlider = createSlider(1,50,10,1);
+    
+    bgColourPicker = createColorPicker('white'); 
+
+    drawRadio = createRadio(); 
+    drawRadio.option('line'); 
+    drawRadio.option('circle'); 
+    drawRadio.option('square'); 
+    drawRadio.option('triangle'); 
+    //default
+    drawRadio.selected('Line');
+
+//for the refresh button
+    var bgColorButton = createButton('Refresh Canvas');
+    bgColorButton.mousePressed(repaint);
+    bgColourPicker.changed( repaint ); 
+    background( bgColourPicker.value() );
+}
+
+function draw(){
+    strokeWeight( strokeWeightSlider.value() );
+    stroke( colourPicker.value() );   
+
+    // remixed from p5js.org/reference/mouseispressed/
+    // https://p5js.org/reference/
+    if(mouseIsPressed){
+        let mode = drawRadio.value();
+        if (mode === 'line'){
+            line(mouseX, mouseY, pmouseX, pmouseY);
+        }  else if (mode === 'circle') { 
+            circle(mouseX, mouseY, 50, 50); 
+        } else if (mode === 'square') { 
+            square(mouseX, mouseY, 50,); 
+        } else if (mode === 'triangle') { 
+            triangle(mouseX, mouseY, mouseX - 30, mouseY + 40, mouseX + 30, mouseY + 40)
+        }
+        
+    }
+}
+    /// end remix
+
+
+
+function repaint(){
+    background( bgColourPicker.value() );
+    console.log( bgColourPicker.value().setGreen(255) );
+}
