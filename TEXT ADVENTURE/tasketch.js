f10fa787e96d25c34e9a9e2afa81fe0285c3017f
4mm3 <emmaferreira@ocadu.ca>
2025-03-20 18:07:19 -0400

    HELP
    this is so hard

--- TEXT ADVENTURE/tasketch.js
+++ TEXT ADVENTURE/tasketch.js
@@ -2,66 +2,64 @@ var input;
 var button;
 var positions = [];
 var name = "";
-var bgColor = 100; // Default background color
+var bgColor;
 var radio;
 
-//THIS SI FOR THE TEXT INPUT STUFF
 function setup() {
-  createCanvas(400, 400);
 
-  // Input and button setup
+  createCanvas(200, 200);
+  bgColor = color(255);
+
+  // INPUT SETUP STUFF
   input = createInput();
-  input.position((width - input.width) / 2, height / 2 - 40);
+  input.position((windowWidth - input.width) / 2, height + 460);
   button = createButton("submit");
-  button.position((width - button.width) / 2, height / 2);
+  button.position((windowWidth - button.width) / 2, height + 500);
   button.mousePressed(startAnimation);
 
-  // RDIOBUTTON SETUP
+  // THIS TOOK M E SO LONG TO FIGURE OUT CAUSE THE COLORS WERE NOT WORKING CAUSE I RESERVSD THE ORDER
   radio = createRadio();
-  radio.option("Gray", 100);
-  radio.option("TOMATO", color(216, 75, 43));
-  radio.option("CREAM", color(255, 253, 208));
-  radio.option("BEEF", color(109, 68, 59));
-  radio.position((width - radio.width) / 2, height / 2 + 40);
-  radio.selected(100); // Default selection
-
-  background(100);
-  noStroke();
-  textAlign(CENTER, CENTER);
-  textSize(20);
-  text("Enter your soup letters.", width / 2, height / 2 - 80);
-}
-
-//THIS IS FOR THE ANIMATION OF THE MOVING SOUP WORDS
-function startAnimation() {
-  name = input.value();
-  positions = []; // Clear previous positions
-  for (let i = 0; i < 30; i++) {
-    positions.push({
-      x: random(width),
-      y: random(height),
-      xSpeed: random(-2, 1),
-      ySpeed: random(-1, 1),
-    });
-  }
+  radio.option("rgb(37, 114, 37)", "veggie"); 
+  radio.option("rgb(139, 69, 19)", "beef");
+  radio.option("rgb(175, 53, 42)", "tomato");
+  radio.position((windowWidth - radio.width) / 2, height - 150);
+  radio.selected("white");
 }
 
 function draw() {
-  background(100);
-  textSize(30);
-  fill(255);
+  let selectedColor = radio.value(); 
+  if (selectedColor) {
+    bgColor = color(selectedColor); 
+  }
+  background(bgColor);
 
-  // ANIMATE TEXT
+//FOR THE TEXT ANIMATION MOVING TEXT
+//IDK WHY NAME IS CROSSED OUT
+  textSize(30);
+  fill(245, 245, 220);
   for (let i = 0; i < positions.length; i++) {
     let pos = positions[i];
     text(name, pos.x, pos.y);
 
-    // Update position
     pos.x += pos.xSpeed;
     pos.y += pos.ySpeed;
 
-    // FOR BOUNCING OFF THE WALLS
+    // FOR THE BOUNCE
     if (pos.x < 0 || pos.x > width) pos.xSpeed *= -1;
     if (pos.y < 0 || pos.y > height) pos.ySpeed *= -1;
+  }
+}
+
+function startAnimation() {
+  name = input.value();
+  positions = []; 
+
+  for (let i = 0; i < 30; i++) {
+    positions.push({
+      x: random(width), 
+      y: random(height), 
+      xSpeed: random(-2, 2), // HORIZONTAL
+      ySpeed: random(-2, 2), // VERTICAL
+    });
   }
 }
